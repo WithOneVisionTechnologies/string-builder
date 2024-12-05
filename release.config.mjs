9027ff304e4cecc3e7bde4fb079a80211937beeb
Doug Shontz, Jr. <doug@shontz.net>
2024-12-05 09:58:38 -0500

    fix(repo): Fix for changelog updating.

--- release.config.mjs
+++ release.config.mjs
@@ -8,6 +8,8 @@ export default {
          {
             changelogFile: "CHANGELOG.md",
          },
+      ],
+      [
          "@semantic-release/changelog",
          {
             changelogFile: "./dist/CHANGELOG.md",
